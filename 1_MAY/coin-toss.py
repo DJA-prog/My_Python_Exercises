6ac9da1caeb2897f677d67aed3ea1e839e9b92a7
DJA-prog <almirall.dino@gmail.com>
2021-05-01 14:10:07 +0200

    update

---
+++ 1_MAY/coin-toss.py
@@ -0,0 +1,16 @@
+go = 0
+while go == 0:
+    # Remember to use the random module
+    import random
+
+    #Don't change the code below
+    coin_seed = int(input("Create a see number: "))
+    random.seed(coin_seed)
+    #Don't change the code above
+
+    #You code goes below
+    if (random.randint(0, 1) == 0):
+        print("Tails")
+    else:
+        print("Heads")
+        
--- 1_MAY/main.py
+++ 1_MAY/main.py
@@ -1,1 +1,21 @@
+# imports
 import module_1
+import random
+
+# module_1
+print(f"PI = {module_1.pi}\n")
+
+# random
+random_int = random.randint(1, 20)
+print(f"Random integer = {random_int}\n")
+
+random_int = random.random()
+print(f"Random float = {random_int}\n")
+
+random_int = random.random()*5
+print(f"Random float integer = {random_int}\n")
+
+random.seed(321)
+random_int = random.random()*5
+print(f"Random seed = {random_int}\n")
+
