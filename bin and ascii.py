68a475f30f882a44be8b42772e57447e97747a5b
Dino Jose Almirall <almirall.dino@gmail.com>
2021-05-18 11:17:17 +0200

    Update bin and ascii.py

--- bin and ascii.py
+++ bin and ascii.py
@@ -1,8 +1,9 @@
 import binascii
 #ascii to bin
-bina = bin(int(binascii.hexlify('hello'), 16))
-print(bina)
+#a_byte_array = bytearray('hello', "utf8")
+#bin(int(binascii.hexlify(a_byte_array), 16))
+#print(bin(int(binascii.hexlify('hello'), 16)))
 #bin to ascii
-n = int('00110100001100101011011000110110001101111', 2)
+n = int('0b110100001100101011011000110110001101111', 2)
 text = binascii.unhexlify('%x' % n)
 print(text)
