bf3996a17128e907c546db647378d157ad666317
Dino Jose Almirall <almirall.dino@gmail.com>
2021-06-26 13:16:27 +0200

    update

--- 12_JUNE/pythonProject/random _walk.py
+++ 12_JUNE/pythonProject/random _walk.py
@@ -3,19 +3,19 @@ import random
 import my_turtle
 
 shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-ortho = [0, 90, 180, 360]
+ortho = [0, 90, 180, 270, 360]
 lengt = [0, 20, -20]
 
 timmy = turtle.Turtle()
 screen = turtle.Screen()
 
+timmy.pensize(5)
 for x in range(1000):
     timmy.shape(random.choice(shapes))
     timmy.right(random.choice(ortho))
     timmy.forward(random.choice(lengt))
     screen.colormode(255)
     timmy.pencolor(my_turtle.random_color())
-    timmy.pensize(5)
 
 screen.exitonclick()
 
