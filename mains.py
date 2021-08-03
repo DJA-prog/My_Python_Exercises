d87f3adb13f53393be766ad370d9d755c5c20579
Dino Jose Almirall <almirall.dino@gmail.com>
2021-08-03 09:57:07 +0200

    Create mains.py

---
+++ mains.py
@@ -0,0 +1,7 @@
+class Author(object):
+    """docstring for Author."""
+
+    def __init__(self, name, email, gender):
+        self.name = name
+        self.email = email
+        self.gender = gender
